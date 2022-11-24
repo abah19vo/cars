8e1c06a2b27ae56cf2b706867258b2bfa0e6212b
Ahmad adnan Abdullah <70108548+abah19vo@users.noreply.github.com>
2022-11-24 17:54:56 +0100

    set the ground functions for the cars details controller

--- cars_api/src/cars-details/cars-details.controller.ts
+++ cars_api/src/cars-details/cars-details.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Param, Post, Put, Res } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { CarsDetails } from 'src/models/car-details';
+import { CarsDetailsService } from './cars-details-service/cars-details.service';
 
 @Controller('cars-details')
-export class CarsDetailsController {}
+export class CarsDetailsController {
+
+     constructor(private carsDetailsService: CarsDetailsService) { }
+
+     @Get()
+     getAll(): CarsDetails[] {
+          return [];
+     }
+
+     @Get(':id')
+     @HttpCode(201)
+     getCarById(@Param('id') id: string): CarsDetails {
+          return {} as CarsDetails;
+     }
+
+     @Post()
+     @HttpCode(201)
+     getCarBy(@Body() createCatDto: CarsDetails) {
+
+     }
+     @Put(':id')
+     @HttpCode(204)
+     updateCarById(@Param('id') id: string, @Body() createCatDto: CarsDetails) {
+          return {} as CarsDetails;
+     }
+}
