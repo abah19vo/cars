10f3a55a90ebd9d934a2ed743248443ee9cff296
Ahmad adnan Abdullah <70108548+abah19vo@users.noreply.github.com>
2022-11-26 11:59:46 +0100

    added view into route

--- cars_website/src/app/app-routing.module.ts
+++ cars_website/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarsViewComponent } from './cars-view/cars-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/cars',
+    pathMatch: 'full'
+  },
+
+  {
+    path:'/cars',
+    component: CarsViewComponent,
+
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
